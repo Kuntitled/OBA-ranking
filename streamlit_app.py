951a0012fb02eb80944d68fe6c61df2f61e515db
Kuntitled <46677308+Kuntitled@users.noreply.github.com>
2025-04-03 19:45:00 +0000

    asdas

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,6 @@ from datetime import date
 
 today = str(date.today())
 
-st.title("Organização de Beyblade do Amazonas " + today )
+st.title("Organização de Beyblade do Amazonas ")
+st.wrrite(today)
 st.title("RANKING ATUAL")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
