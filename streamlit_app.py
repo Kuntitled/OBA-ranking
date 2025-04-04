f318befd91e2c68ebf0ee5b0c72a438920f878c4
Kuntitled <46677308+Kuntitled@users.noreply.github.com>
2025-04-04 14:27:28 +0000

    added gsheets integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,20 @@
 import streamlit as st
 from datetime import date
+from streamlit_gsheets import GSheetsConnection
 
+gSheetsConnection = st.connection("gsheets", type=GSheetsConnection)
+df = gSheetsConnection.read()
+
+
+st.set_page_config(page_title="Ranking - OBA", 
+                   page_icon=":shark:"
+                   )
 
 today = str(date.today())
 
-st.title("Organização de Beyblade do Amazonas ")
+st.title("Organização de Beyblade do Amazonas")
 st.write(today)
 st.title("RANKING ATUAL")
+
+for row in df.itertuples():
+    st.write(f"{row.BLADER} tem {row.PONTOS} e está em {row.COLOCAÇÃO}")
