3d4458100ba835005e2e78164d2ef70efe2b5c18
Kuntitled <46677308+Kuntitled@users.noreply.github.com>
2025-04-09 20:18:08 +0000

    Still messing with the sheet integration

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,7 @@ from streamlit_gsheets import GSheetsConnection
 gSheetsConnection = st.connection("gsheets", type=GSheetsConnection)
 df = gSheetsConnection.read()
 
+mainTable = []
 
 st.set_page_config(page_title="Ranking - OBA", 
                    page_icon=":shark:"
@@ -16,5 +17,10 @@ st.title("Organização de Beyblade do Amazonas")
 st.write(today)
 st.title("RANKING ATUAL")
 
-for row in df.itertuples()[:3]:
-    st.write(f"{row.blader} tem {row.points} e está em {row.placement}")
+#for row in df.itertuples()[:3]:
+#    st.write(f"{row.blader} tem {row.points} e está em {row.placement}")
+
+for row in df.intertuples():
+    mainTable.append(row)
+
+st.write(mainTable)
